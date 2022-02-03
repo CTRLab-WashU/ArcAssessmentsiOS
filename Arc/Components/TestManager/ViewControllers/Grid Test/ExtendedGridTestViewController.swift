fc7819966d113965a4e90781679f186d8c374a17
Mike DePhillips <mike@sdpdigital.com>
2022-02-03 13:45:37 -0800

    Update grid test waiting time

--- Arc/Components/TestManager/ViewControllers/Grid Test/ExtendedGridTestViewController.swift
+++ Arc/Components/TestManager/ViewControllers/Grid Test/ExtendedGridTestViewController.swift
@@ -253,23 +253,27 @@ open class ExtendedGridTestViewController: ArcViewController, UICollectionViewDe
 
 		
 	}
+    
     open func displayReady()
     {
         
-		guard isVisible else {
-			return
-		}
-
-		currentAlert = Arc.shared.displayAlert(message: "Ready".localized(ACTranslationKey.grids_overlay3_pt2), options: [.wait(waitTime: 1.0, {
-			[weak self] in
-			self?.displayGrid()
-//			if let s = self {
-//				s.tapOnTheFsLabel.isHidden = true
-//			}
-			$0.removeFromSuperview()
-		})])
-
-		
+        guard isVisible else {
+            return
+        }
+        
+        Arc.shared.displayAlert(message: "".localized(ACTranslationKey.grids_overlay3), options: [
+            .wait(waitTime: 2.0, {
+            
+                $0.set(message:"".localized(ACTranslationKey.grids_overlay3_pt2))
+        }),
+        .wait(waitTime: 4.0, {
+            [weak self] in
+            self?.displayGrid()
+            if let s = self {
+                s.tapOnTheFsLabel.isHidden = true
+            }
+            $0.removeFromSuperview()
+        })])
     }
     
     open func displayPreSymbols()
