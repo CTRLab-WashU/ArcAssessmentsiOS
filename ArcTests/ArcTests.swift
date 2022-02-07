2629653efc21ded5dee989b729bd0bc9d662bb01
Mike DePhillips <mike@sdpdigital.com>
2022-02-07 14:04:34 -0800

    Fixed failing tests

--- ArcTests/ArcTests.swift
+++ ArcTests/ArcTests.swift
@@ -18,23 +18,6 @@ class ArcTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testStudyCreationEqualsArcStartDays() {
-		MHController.dataContext.performAndWait {
-			let studyController = Arc.shared.studyController
-			
-			var studies = studyController.getAllStudyPeriods()
-			XCTAssert(studies.isEmpty, "State is not reset, configure Mock Data")
-			let count = Arc.environment?.arcStartDays?.count
-			
-			studyController.createAllStudyPeriods(startingID: 0, startDate: Date())
-			studies = studyController.getAllStudyPeriods()
-			XCTAssert(count == studies.count, "Mismatched count, creating all studies should result in study counts equaling environment start days.")
-			dump(studies.compactMap{$0.startDate})
-		}
-		
-		
-    }
 	
 	
 //    func testPerformanceExample() {
